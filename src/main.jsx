0118c5c023b4487b0c428b23f4514b1f1c0aa199
Javier Gonzalez <javgon2306@gmail.com>
2024-09-10 15:38:28 -0300

    component Message

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import { createRoot } from 'react-dom/client'
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { HooksApp } from "./assets/HooksApp"
 import './assets/index.css'
-import { CounterApp } from './01-usState/CounterApp'
-import { CounterCustomHook } from './01-usState/CounterCustomHook'
+// import { CounterApp } from './01-usState/CounterApp'
+// import { CounterCustomHook } from './01-usState/CounterCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
+  {/* // <StrictMode> */}
     {/* <HooksApp /> */}
     {/* <CounterApp/> */}
-    <CounterCustomHook/>
-  </StrictMode>,
+    {/* <CounterCustomHook/> */}
+    <SimpleForm/>
+  {/* </StrictMode>, */}
+  </>
 )
