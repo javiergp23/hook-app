393b59a1f36c5ce4f4bc41f833ed5b146e047c6d
Javier Gonzalez <javgon2306@gmail.com>
2024-09-10 15:55:09 -0300

    FormWithCustomHook

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { createRoot } from 'react-dom/client'
 // import { StrictMode } from 'react'
-import { HooksApp } from "./assets/HooksApp"
+// import { HooksApp } from "./assets/HooksApp"
 import './assets/index.css'
 // import { CounterApp } from './01-usState/CounterApp'
 // import { CounterCustomHook } from './01-usState/CounterCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 
 createRoot(document.getElementById('root')).render(
   <>
@@ -12,7 +13,7 @@ createRoot(document.getElementById('root')).render(
     {/* <HooksApp /> */}
     {/* <CounterApp/> */}
     {/* <CounterCustomHook/> */}
-    <SimpleForm/>
+    <FormWithCustomHook/>
   {/* </StrictMode>, */}
   </>
 )
