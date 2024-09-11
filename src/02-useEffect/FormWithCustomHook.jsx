3c4f7b6b73fef076091c50b28f5a26633d7e33f3
Javier Gonzalez <javgon2306@gmail.com>
2024-09-11 11:59:20 -0300

    2

--- src/02-useEffect/FormWithCustomHook.jsx
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -3,15 +3,11 @@ import { useForm } from "../hooks/useForm";
 
 export const FormWithCustomHook = () => {
 
-   const { formState, onInputChange } = useForm({
+   const { formState, onInputChange, onResetForm, username, email, password } = useForm({
        username: '',
        email: '',
        password: ''
    })
-
-   const { username, email, password } = formState
-
-   
 
     return(
         <>
@@ -42,6 +38,8 @@ export const FormWithCustomHook = () => {
                 value={password}
                 onChange={onInputChange}
             />
+
+            <button onClick={ onResetForm } className="btn btn-primary">Borrar</button>
         </>
     )
 }
