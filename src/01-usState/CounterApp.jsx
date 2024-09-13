3310ba4742200d6b88548b1dbb6e65a9c0ec7b5d
Javier Gonzalez <javgon2306@gmail.com>
2024-09-13 20:25:52 -0300

    CounterApp fix

--- src/01-usState/CounterApp.jsx
+++ src/01-usState/CounterApp.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 export const CounterApp = () => {
-
     const [state, setCounter] = useState({
         counter1: 10,
         counter2: 20,
@@ -10,8 +9,6 @@ export const CounterApp = () => {
     });
 
     const {counter1, counter2, counter3} = state;
-
-
   return(
     <>
     <h1>Counter1: {counter1}</h1>
